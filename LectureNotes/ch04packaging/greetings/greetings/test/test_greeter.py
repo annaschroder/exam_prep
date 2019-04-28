3b6e58f448457559d5082f88ef29fdab8189e6c2
annaschroder <45434483+annaschroder@users.noreply.github.com>
2019-04-28 21:20:21 +0100

    Add files via upload

---
+++ LectureNotes/ch04packaging/greetings/greetings/test/test_greeter.py
@@ -0,0 +1,17 @@
+import yaml
+import os
+import pytest
+from ..greeter import greet
+
+def read_fixture():
+    with open(os.path.join(os.path.dirname(__file__),
+                           'fixtures',
+                           'samples.yaml')) as fixtures_file:
+        fixtures = yaml.load(fixtures_file)
+    return fixtures
+
+@pytest.mark.parametrize("fixture", read_fixture())
+def test_greeter(fixture):
+    answer = fixture.pop('answer')
+    assert greet(**fixture) == answer
+    
---
+++ LectureNotes/ch04packaging/index.md
@@ -0,0 +1,11 @@
+---
+title: Software Projects
+---
+
+* Turning your code into a package
+* Releasing code
+* Documentation
+* Software project management
+* Organising issues and tasks
+* Choosing an open-source license
+
